66537288cb99cc18f350f1642ca2892690895147
rajvi01 <rajvi.vashist@gmail.com>
2022-09-11 22:25:12 +0530

    docker files

---
+++ spring-dockerhub-maven/src/main/java/com/springdockerhubmaven/SpringDockerhubMavenApplication.java
@@ -0,0 +1,13 @@
+package com.springdockerhubmaven;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDockerhubMavenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDockerhubMavenApplication.class, args);
+	}
+
+}
